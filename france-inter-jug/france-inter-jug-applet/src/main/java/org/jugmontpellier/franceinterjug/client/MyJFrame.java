c3f81a20956a137394a8b0e6bee20dcf44db477d
olivier.nouguier <olivier.nouguier@gmail.com>
2010-12-30 12:50:25 +0100

    Test GitADD with EGit

---
+++ france-inter-jug/france-inter-jug-applet/src/main/java/org/jugmontpellier/franceinterjug/client/MyJFrame.java
@@ -0,0 +1,12 @@
+package org.jugmontpellier.franceinterjug.client;
+
+import javax.swing.JFrame;
+
+public class MyJFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
